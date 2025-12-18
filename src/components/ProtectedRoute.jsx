280cfb2f3050a1d2662c3177410c2aff06b8dc5f
0110553616 <zahra.alimohammadi@email.kntu.ac.ir>
2025-12-18 22:43:45 +0330

    refactor: code cleanup and user access handling

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ authenticated, children }) => {
-  if (!authenticated) {
-    alert("برای دسترسی به این بخش وارد حساب کاربری شوید");
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    if (!authenticated) {
+      alert("برای دسترسی به این بخش وارد حساب کاربری شوید");
+      setRedirect(true); 
+    }
+  }, [authenticated]);
+
+  if (redirect) {
     return <Navigate to="/" replace />;
   }
 
   return children;
 };
 
-export default ProtectedRoute
+export default ProtectedRoute;
