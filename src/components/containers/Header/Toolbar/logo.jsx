e2e94e1b6e27a04c4e3bf910122a78a17f23a0f2
0110553616 <zahra.alimohammadi@email.kntu.ac.ir>
2025-12-18 22:24:40 +0330

    refactor: code cleanup and user access handling

--- src/components/containers/Header/Toolbar/logo.jsx
+++ src/components/containers/Header/Toolbar/logo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import reactLogo from '../../../../assets/react.svggit '
+import reactLogo from '../../../../assets/react.svg'
 import './logo.css'
 const Logo = ()=> {
     return (
